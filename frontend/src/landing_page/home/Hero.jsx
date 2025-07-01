6bfa3d34b53dad1e69d491d19b49572c5159b7a3
Rahul Kumar Tiwari <irktiwari@gmail.com>
2025-07-01 14:59:36 +0530

    added a signup button on homepage

--- frontend/src/landing_page/home/Hero.jsx
+++ frontend/src/landing_page/home/Hero.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function Hero() {
+    const navigate = useNavigate();
+    function handleButtonClick() {
+        navigate("/signup");
+    }
     return ( 
         <>
             <div className='container p-5'>                                                             
@@ -9,7 +14,7 @@ function Hero() {
                     <img src='media/images/homeHero.png' alt='Hero Image' className='mb-5' />
                     <h1 className='mt-5'>Invest in everything</h1>
                     <p>Online platform to invest in stocks, derivatives, mutual funds, and more</p>
-                    <button className='p-2 btn btn-primary fs-5 mb-5' style={{width:"20%", margin: "0 auto"}}>Sign up for free</button>
+                    <button onClick={handleButtonClick} className='p-2 btn btn-primary fs-5 mb-5' style={{width:"20%", margin: "0 auto"}}>Sign up for free</button>
                 
                 </div>
             </div>
